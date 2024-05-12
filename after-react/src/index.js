a3de3f76246629d67018787bf5ba62a8e426ed58
kimtimo <eogus8875@naver.com>
2024-05-12 17:38:01 +0900

    feat : element 정의 및 render 학습

--- after-react/src/index.js
+++ after-react/src/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { render } from 'react-dom/client';
 import './index.css';
-import App from './App';
+// import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Library from './chap_03/Library';
+import Clock from './chap_04/Clock';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Library />
+    <Clock />
   </React.StrictMode>,
   document.getElementById('root')
 );
