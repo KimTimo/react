b3ef8d346b7a41681bef59b58e85d59bf150855d
kimtimo <eogus8875@naver.com>
2024-05-13 16:11:15 +0900

    add : useState 변경, array copy 문법 학습

--- coding-apple/src/App.tsx
+++ coding-apple/src/App.tsx
@@ -1,28 +1,33 @@
 import React, { useState } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 const App = () => {
-  let logo: string = 'bizbee';
-  let title: string = 'ERP 3팀 진행중 프로젝트';
-  let [project, a] = useState(['ERP Mobile', '물류화랑 Mobile', '영업&물류']);
+  let logo: string = '지콩홈';
+  let [title, setTitle] = useState('지콩가족');
+  let [member, setMember] = useState(['안자몽', '안몽쉘', '안꼬비']);
+  let [isLike, addLike] = useState(0);
 
   return (
     <div className="App">
       <div className="black-nav">
         <h4>{logo}</h4>
       </div>
+      <button onClick={() => {
+        let copyMember = [...member];
+        copyMember[0] = '김오뜨';
+          setMember(copyMember);
+        }} >버튼임</button>
       <div className="list">
-        <h4>{title}</h4>
-        <p>{project[0]}</p>
+        <h4>{ title }<span onClick={() => { addLike(isLike++); }}> {' '}👍</span>{isLike}</h4>
+        <p>{member[0]} </p>
       </div>
       <div className="list">
         <h4>{title}</h4>
-        <p>{project[1]}</p>
+        <p>{member[1]}</p>
       </div>
       <div className="list">
         <h4>{title}</h4>
-        <p>{project[2]}</p>
+        <p>{member[2]}</p>
       </div>
     </div>
   );
