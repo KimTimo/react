5f57c802ff468d2054ae89bb3e7db66298e4d820
kimtimo <eogus8875@naver.com>
2024-05-14 16:46:22 +0900

    add : component, props 정의 학습

--- coding-apple/src/component/CommentList.jsx
+++ coding-apple/src/component/CommentList.jsx
@@ -1,10 +1,27 @@
 import React from 'react';
 import Comment from './Comment';
 
+const comments = [
+  {
+    name: '김머콩',
+    comment: '내 이름은 김머콩이다.',
+  },
+  {
+    name: '안지콩',
+    comment: '심술 대마왕 안지콩',
+  },
+  {
+    name: '안꼬비',
+    comment: '왕!',
+  },
+];
+
 const CommentList = (props) => {
   return (
     <div>
-      <Comment />
+      {comments.map((comment) => {
+        return <Comment name={comment.name} comment={comment.comment} />;
+      })}
     </div>
   );
 };
